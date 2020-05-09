2c1a905960f37896519f704bc3749f7ac76033dd
Luv Gangwani <luvgangwani>
2020-05-09 20:27:50 +1000

    Lesson 17 - Vue JS CLI

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <p>{{ greetings() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Vue JS Using CLI'
+    }
+  },
+  methods: {
+    greetings() {
+      return 'Welcome to the first Vue JS application created using CLI';
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
