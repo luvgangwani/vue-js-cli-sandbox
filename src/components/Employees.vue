8b0ff5c0b55a70a6d8d1148bd3050bae625e9e05
Luv Gangwani <luvgangwani>
2020-05-12 00:04:07 +1000

    Lesson 26 - Lifecycle hooks

--- src/components/Employees.vue
+++ src/components/Employees.vue
@@ -27,7 +27,31 @@ export default {
         return {
 
         };
-    }
+    },
+
+    beforeCreate() {
+        alert('beforeCreate');
+    },
+
+    created() {
+        alert('created');
+    },
+
+    beforeMount() {
+        alert('beforeMount');
+    },
+
+    mounted() {
+        alert('mounted');
+    },
+
+    beforeUpdate() {
+        alert('beforeUpdate');
+    },
+
+    updated() {
+        alert('updated');
+    },
 
 }
 </script>
